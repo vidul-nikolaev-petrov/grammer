9ef3fe542c731c7f0c22a80d1f96175c432c5711
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2026-04-17 16:51:48 +0300

    Refactor preload the data

--- generate.js
+++ generate.js
@@ -1,14 +1,25 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Load indexed data
-const dataModule = require('./dictionaryData.js');
+const dataModule = require("./dictionaryData.js");
 
 // Load HTML template
-let template = fs.readFileSync('./template.html', 'utf8');
+let template = fs.readFileSync("./template.html", "utf8");
 
 // Inject and save
-const jsonData = JSON.stringify(dataModule);
-const finalHtml = template.replace('const dictionary = [];', `const dictionary = ${jsonData};`);
+const jsonData = JSON.stringify(
+    dataModule.map((i) => ({
+        ...i,
+        lowT: i.t.toLowerCase(),
+    })),
+);
 
-fs.writeFileSync('./index.html', finalHtml);
-console.log(`Standalone search engine created with ${dataModule.length} records.`);
+const finalHtml = template.replace(
+    "const dictionary = ['placeholder'];",
+    `const dictionary = ${jsonData};`,
+);
+
+fs.writeFileSync("./index.html", finalHtml);
+console.log(
+    `Standalone search engine created with ${dataModule.length} records.`,
+);
